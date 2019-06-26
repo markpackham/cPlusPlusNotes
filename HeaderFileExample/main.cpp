263048422b1b7da25e4e6a2947cbb6dfb65bd697
Mark Packham <markpackham1@gmail.com>
2019-06-26 18:34:51 +0100

    header file notes

---
+++ HeaderFileExample/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "fns.hpp"
+
+//header files are for function declrations so the main method doesn't get bloaded
+int main()
+{
+
+    std::cout << is_palindrome("noon") << "\n";
+    std::cout << tenth_power(4) << "\n";
+    std::cout << average(4.0, 7.0) << "\n";
+}
